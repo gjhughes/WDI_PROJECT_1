e1e532c6c1e0d59850cd0d238cc96a4fb351ccac
gjhughes <ghughes@me.com>
2017-10-09 23:05:06 +0100

    tiles changing colour on click

--- js/app.js
+++ js/app.js
@@ -1,40 +1,51 @@
-$(document).ready(function() {
+$(() => {
 
   const $tiles = $('.tiles');
-  console.log($tiles)
-  const randomNumbers = [];
-  let pattern;
+  console.log($tiles);
+  let $clicked;
+  const randomPattern = [];
+  const userPattern = [];
 
   // random number function
   function randNum() {
-    for (let i = 0; randomNumbers.length < 6; i++) {
+    for (let i = 0; randomPattern.length < 6; i++) {
       const num = Math.floor(Math.random() * 16);
-      if(randomNumbers.includes(num)) {
+      if(randomPattern.includes(num)) {
         console.log('number in array already');
       } else {
-        randomNumbers.push(num);
+        randomPattern.push(num);
       }
-      randomNumbers.sort((a, b) => a - b);
+      randomPattern.sort((a, b) => a - b);
     }
   }
 
   // light up tiles based on random number array
   function displayTile() {
-    for (let i = 0; i < randomNumbers.length; i++) {
-      const randomNumber = randomNumbers[i];
+    for (let i = 0; i < randomPattern.length; i++) {
+      const randomNumber = randomPattern[i];
       const $element = $($tiles[randomNumber]);
       setTimeout(function() {
         $element.css('background', '#F0F1F2');
         setTimeout(function(){
           $element.css('background', '#6E7783');
-        }, 1000);
-      }, i*1000 + 1000);
+        }, 500);
+      }, i*500 + 500);
     }
   }
 
+  // create array from users clicked tiles
+  function clickHandler() {
+    $tiles.on('click', function(e) {
+      $clicked = $(e.target);
+      $clicked.css('background', '#77AAAD');
+      console.log($clicked);
+    });
+  }
+  clickHandler();
+
   randNum();
   displayTile();
-  console.log(randomNumbers);
+  console.log(randomPattern);
 
 });
 
