25570401513334d476188d9b196dc0ae793ba9d7
gjhughes <ghughes@me.com>
2017-10-12 16:59:35 +0100

    updated styles

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ let userClicks        = 0;
 let computerPattern   = [];
 let userPattern       = [];
 
-
 $(init);
 
 function init() {
@@ -25,15 +24,18 @@ function init() {
   $score   = $('.score');
   $play    = $('.play-btn');
   $restart = $('.restart-btn');
+
   $restart.hide();
   $tiles.hide();
+
   $('button').on('click', startGame);
+
 }
 
 function startGame() {
 
-  score = 0;
   lives = 3;
+  score = 0;
   level = 0;
   setTimeout(function() {
     displayTiles();
@@ -44,10 +46,38 @@ function startGame() {
     }, 500);
   }, 500);
 
+}
+
+function gameOver() {
+
+  $grid.addClass('game-over');
+  // $tiles.hide();
+  $grid.html('Game Over!');
+  $grid.append($restart.show());
+  $restart.on('click', reset);
+
+}
+function reset(){
+
+  $restart.hide();
+  $grid.removeClass('game-over');
+  $grid.html('');
+  $grid.addClass('grid').show();
+  startGame();
+
+}
+function displayTiles() {
+
+  $play.hide();
+  $grid.removeClass('start-grid');
+  setTimeout(function() {
+    $tiles.fadeIn();
+  }, 250);
 
 }
 
 function nextLevel() {
+
   $tiles.off('click');
 
   $tiles.on('mouseover', function(e) {
@@ -69,43 +99,30 @@ function nextLevel() {
       gameOver();
     }, 1500);
   }
+
 }
 
 function updateScoreboard() {
+
   level++;
   $level.html(level);
   $lives.html(lives);
   $score.html(score);
-}
-
-function gameOver() {
-  $grid.addClass('game-over');
-  $tiles.hide();
-  $grid.html('Game Over!');
-  $grid.append($restart.show().addClass('restart-btn').html('Play Again'));
-  $('button').on('click', startGame);
-
-  console.log($restart);
-}
-
-function displayTiles() {
-  $play.hide();
-  $grid.removeClass('start-grid');
-  setTimeout(function() {
-    $tiles.fadeIn();
-  }, 250);
 
 }
 
 function generatePattern() {
+
   for (let i = 0; computerPattern.length < patternLength; i++) {
     const num = Math.floor(Math.random() * 16).toString();
     if(!computerPattern.includes(num))
       computerPattern.push(num.toString());
   }
+
 }
 
 function displayPattern() {
+
   for (let i = 0; i < computerPattern.length; i++) {
     const randomNumber = computerPattern[i];
     const $element = $($tiles[randomNumber]);
@@ -116,11 +133,11 @@ function displayPattern() {
 
         if (i + 1 === computerPattern.length) {
           checkMatch();
-
         }
       }, 500);
     }, i*500 + 500);
   }
+
 }
 
 function checkMatch() {
@@ -138,8 +155,6 @@ function checkMatch() {
       userPattern.push($clicked);
       userClicks++;
 
-      console.log(score);
-
       $(e.target).addClass('chosen');
       if (userPattern.length === computerPattern.length) {
         $tiles.off('click');
@@ -150,15 +165,17 @@ function checkMatch() {
       }
     } else {
       $(e.target).addClass('wrong-button');
+
       lives--;
-      console.log(lives);
       // display correct sequence.
       for (let i = 0; i < computerPattern.length; i++) {
         $(`#${computerPattern[i]}`).addClass('wrong-pattern').html(i + 1);
       }
+
       level--;
       patternLength--;
       nextLevel();
     }
   });
+
 }
